0c6b3fa9863cd6ef2c315d8c78a45217026a03fe
kstephens0331 <Kyle_stephens31@icloud.com>
2025-06-03 20:00:50 -0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './assets/logo.png'; // Update path if needed
+import logo from '../assets/logo.png'; // Update path if needed
 
 function Footer() {
   return (
